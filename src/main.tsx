694455d4d7932ccd607771f2cb2c3dad7a58856f
zuya <kazu.prgrm555y@gmail.com>
2025-11-16 15:52:05 +0900

    add: root setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import Create from "./page/Create.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const rooter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create",
+    element: <Create />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={rooter} />
+  </StrictMode>
+);
