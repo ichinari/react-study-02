2bc1b1eccb8f4507cc471fee634c8a9e173b609c
zuya <kazu.prgrm555y@gmail.com>
2025-11-23 23:59:28 +0900

    add: get music list function

--- src/page/List.tsx
+++ src/page/List.tsx
@@ -1,39 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMusic } from "@api/supabase/music";
+import type { MusicColums } from "@api/supabase/types";
 import { useNavigation } from "@hooks/useNavigation";
 
-function App() {
-  // sample list data
-  const musicList = [
-    {
-      id: 1,
-      title: "Synthwave Dreams",
-      artist: "AI Composer",
-      audioUrl:
-        "https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3",
-      coverUrl:
-        "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=400&h=400&fit=crop&crop=center",
-    },
-    {
-      id: 2,
-      title: "Jazz Fusion",
-      artist: "Neural Network",
-      audioUrl:
-        "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3",
-      coverUrl:
-        "https://images.unsplash.com/photo-1511379938547-c1f69419868d?w=400&h=400&fit=crop&crop=center",
-    },
-    {
-      id: 3,
-      title: "Ambient Spaces",
-      artist: "Deep Learning",
-      audioUrl:
-        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/theme_01.mp3",
-      coverUrl:
-        "https://images.unsplash.com/photo-1514320291840-2e0a9bf2a9ae?w=400&h=400&fit=crop&crop=center",
-    },
-  ];
-
+function List() {
   const dammyImageUrl = "https://placehold.jp/3d4070/ffffff/150x150.png?text=";
   const navigate = useNavigation();
+  const [musicList, setMusicList] = useState<MusicColums[]>([]);
+
+  useEffect(() => {
+    const fetchMusic = async () => {
+      const musicList = await getMusic();
+      setMusicList(musicList);
+    };
+    fetchMusic();
+  }, []);
 
   return (
     <div className="p-6">
@@ -66,7 +47,7 @@ function App() {
               <h3 className="font-bold">{music.title}</h3>
 
               <audio controls>
-                <source src={music.audioUrl} type="audio/mpeg" />
+                <source src={music.music_file_path} type="audio/mpeg" />
               </audio>
             </div>
           ))}
@@ -80,4 +61,4 @@ function App() {
   );
 }
 
-export default App;
+export default List;
